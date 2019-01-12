833e62ce15b5dbc02ead9e8a7787127087d5ea6e
Adrian Nirmal Andrew <kingspammernerd@gmail.com>
2019-01-12 12:37:20 +0530

    Updated Encrypter.java
    Currently done:
    -Changed Encrypter to get the actual data, instead of a file name.
    -*Almost* finished implementing the IV creation and encryption process.
    -Finished the password setter.

--- kingcrypter/crypter/Encrypter.java
+++ kingcrypter/crypter/Encrypter.java
@@ -1,5 +1,5 @@
-//This class implements functionality for encrypting files.
-
+//This class implements functionality for encrypting raw data.
+//CONTINUE FROM LINE 87
 package kingcrypter.crypter;
 
 import java.io.*;
@@ -11,13 +11,13 @@ import java.security.spec.*;
 
 /*
 Process:
-1. Open file, get its contents <plainInput> and hash it <hash64>
+1. Read an array of bytes <plainInput> and hash it <hash64>
 2. Generate RSA keypair <rsaPriv>,<rsaPub>
 3. Get current system time (This will be used as AES Initialization Vector) <encTime>
-4. Ask user to enter a password <passwd>,<passwdConfirm>
+4. Ask user to enter a password <passwd>
 5. Encrypt file with (AES/CBC, passwd, encTime) <cipherText64>
 6. Encrypt EncTime with Pub <encTimeRsa64>
-7. Write the first output file as: encTimeRsa64;cipherText64;hash64; and the other output file as <filename>_key with the base64-encoded Priv
+7. Write the first output file as: encIvRsa64;cipherText64;hash64; and the other output file as <filename>_key with the base64-encoded Priv
 */
 
 public class Encrypter {
@@ -33,18 +33,26 @@ public class Encrypter {
 	private long startTime=0, endTime=0;
 	//The hash of their difference is used as the AES Initialization Vector.
 	
+	//Password used:
+	private String passwd=null;
+	//IV for AES encryption:
+	private IvParameterSpec encIv=null;
+	
+	//Output encrypted IV bytes, encoded to base64:
+	private String encIvRsa64=null;
+	//Output ciphertext, encoded to base64:
+	private String cipherText64=null;
+	
 	//RSA KeyPair:
 	private KeyPair keyPair=null;
+	//RSA key size:
+	int rsaKeyLength=0;
 	
-	//Constructor, takes the input file's name:
-	public Encrypter(String inFileName) throws IOException,NoSuchAlgorithmException {
-		//Open file:
-		File inFile=new File(inFileName);
-		//Read file contents:
-		FileInputStream inStream=new FileInputStream(inFile);
-		this.plainInput=new byte[(int)inFile.length()];
-		inStream.read(plainInput);
-		//Hash the file:
+	//Constructor, takes raw data to be encrypted:
+	public Encrypter(byte[] input) throws NoSuchAlgorithmException {
+		//Copy bytes:
+		this.plainInput=input;
+		//Hash the data:
 		this.hash256=MessageDigest.getInstance("SHA-256");
 		byte[] hash=this.hash256.digest(this.plainInput);
 		//Encode hash to base64:
@@ -56,9 +64,33 @@ public class Encrypter {
 		//Check key length:
 		if(!(rsaKeyLength==2048 || rsaKeyLength==4096))
 			throw new RuntimeException("Invalid key length, use 2048 or 4096!");
+		this.rsaKeyLength=rsaKeyLength;
 		//Generate RSA keypair:
 		KeyPairGenerator kpg=KeyPairGenerator.getInstance("RSA");
 		kpg.initialize(rsaKeyLength);
 		this.keyPair=kpg.generateKeyPair();
+	}
+	
+	//Create IvParameterSpec for encryption, using the first 16 bytes of hash of endTime-startTime. Called from setPasswd:
+	//Also, encrypt the IV in this step.
+	private void createIv(long timeDiff) {
+		//Convert timeDiff to String:
+		String diffStr=String.valueOf(timeDiff);
+		//Hash diffStr:
+		byte diffStrHash[]=this.hash256.digest(diffStr.getBytes());
+		//Store first 16 bytes of diffStrHash as IvParameterSpec:
+		this.encIv=new IvParameterSpec(Arrays.copyOf(diffStrHash, 16));
+		//Encrypt the IV:
+		Cipher enc=Cipher.getInstance("RSA");
+		enc.init(this.rsaKeyLength);
+		byte[] encIvRsa=enc.doFinal(Cipher.ENCRYPT_MODE, this.keyPair.getPublic());
+		//CONTINUE FROM HERE
+	}
+	
+	//Get and store password. Confirmation of password is to be done by calling method:
+	public void setPasswd(String passwd) {
+		this.passwd=new String(passwd);
+		this.endTime=System.currentTimeMillis();
+		this.createIv(this.endTime-this.startTime);
 	}
 }
